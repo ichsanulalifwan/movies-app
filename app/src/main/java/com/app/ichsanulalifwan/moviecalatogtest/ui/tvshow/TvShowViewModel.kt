f521a1a5a581cb330febe96642a57793af83c186
ichsanulalifwan <ichsanulalifwan@gmail.com>
2022-10-16 08:01:21 +0800

    update tv show viewModel

--- app/src/main/java/com/app/ichsanulalifwan/moviecalatogtest/ui/tvshow/TvShowViewModel.kt
+++ app/src/main/java/com/app/ichsanulalifwan/moviecalatogtest/ui/tvshow/TvShowViewModel.kt
@@ -6,14 +6,17 @@ import androidx.paging.PagedList
 import com.app.ichsanulalifwan.moviecalatogtest.data.AppRepository
 import com.app.ichsanulalifwan.moviecalatogtest.data.Resource
 import com.app.ichsanulalifwan.moviecalatogtest.data.source.local.entity.tvshow.TvShowAiringEntity
+import com.app.ichsanulalifwan.moviecalatogtest.data.source.local.entity.tvshow.TvShowOnTheAirEntity
+import com.app.ichsanulalifwan.moviecalatogtest.data.source.local.entity.tvshow.TvShowPopularEntity
+import com.app.ichsanulalifwan.moviecalatogtest.data.source.local.entity.tvshow.TvShowTopRatedEntity
 
 class TvShowViewModel(private val repository: AppRepository) : ViewModel() {
 
     fun getAiringTodayTvShow(): LiveData<Resource<PagedList<TvShowAiringEntity>>> = repository.getAiringTodayTvShow()
 
-    fun getOnTheAirTvShow(): LiveData<Resource<PagedList<TvShowAiringEntity>>> = repository.getOnTheAirTvShow()
+    fun getOnTheAirTvShow(): LiveData<Resource<PagedList<TvShowOnTheAirEntity>>> = repository.getOnTheAirTvShow()
 
-    fun getPopularTvShow(): LiveData<Resource<PagedList<TvShowAiringEntity>>> = repository.getPopularTvShow()
+    fun getPopularTvShow(): LiveData<Resource<PagedList<TvShowPopularEntity>>> = repository.getPopularTvShow()
 
-    fun getTopRatedTvShow(): LiveData<Resource<PagedList<TvShowAiringEntity>>> = repository.getTopRatedTvShow()
+    fun getTopRatedTvShow(): LiveData<Resource<PagedList<TvShowTopRatedEntity>>> = repository.getTopRatedTvShow()
 }
